9c9c351d92f6c2d287fa174ec0ee164148a8d5a8
AntonioPizzirusso <71682934+AntonioPizzirusso@users.noreply.github.com>
2021-04-29 19:38:47 -0400

    fasdae

--- index.js
+++ index.js
@@ -1,71 +1,91 @@
-// scripts here:
-
-function submitQuiz() {
-  console.log('submitted');
-
-// get each answer score
-  function answerScore (qName) {
-    var radiosNo = document.getElementsByName(qName);
-
-    for (var i = 0, length = radiosNo.length; i < length; i++) {
-         if (radiosNo[i].checked) {
-    // do something with radiosNo
-        var answerValue = Number(radiosNo[i].value);
-      }
-    }
-    // change NaNs to zero
-    if (isNaN(answerValue)) {
-      answerValue = 0;
-    }
-    return answerValue;
-  }
-
-// calc score with answerScore function
-  var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-  console.log("CalcScore: " + calcScore); // it works!
-
-// function to return correct answer string
-  function correctAnswer (correctStringNo, qNumber) {
-    console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-    return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
-      (document.getElementById(correctStringNo).innerHTML) + "</strong>");
-    }
-
-// print correct answers only if wrong (calls correctAnswer function)
-  if (answerScore('q1') === 0) {
-    document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-  }
-  if (answerScore('q2') === 0) {
-    document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-  }
-  if (answerScore('q3') === 0) {
-    document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-  }
-  if (answerScore('q4') === 0) {
-    document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
-  }
-
-// calculate "possible score" integer
-  var questionCountArray = document.getElementsByClassName('question');
-
-  var questionCounter = 0;
-  for (var i = 0, length = questionCountArray.length; i < length; i++) {
-    questionCounter++;
-  }
-
-// show score as "score/possible score"
-  var showScore = "Your Score: " + calcScore +"/" + questionCounter;
-// if 4/4, "perfect score!"
-  if (calcScore === questionCounter) {
-    showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-  };
-  document.getElementById('userScore').innerHTML = showScore;
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
 }
 
-$(document).ready(function() {
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.questionIndex];
+}
 
-$('#submitButton').click(function() {
-  $(this).addClass('hide');
-});
+Quiz.prototype.guess = function(answer) {
+  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+  }
 
-});
+  this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+  return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
+}
+
+
+function populate() {
+  if(quiz.isEnded()) {
+      showScores();
+  }
+  else {
+      // show question
+      var element = document.getElementById("question");
+      element.innerHTML = quiz.getQuestionIndex().text;
+
+      // show options
+      var choices = quiz.getQuestionIndex().choices;
+      for(var i = 0; i < choices.length; i++) {
+          var element = document.getElementById("choice" + i);
+          element.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
+
+      showProgress();
+  }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+      quiz.guess(guess);
+      populate();
+  }
+};
+
+
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+  new Question("How many sides does a hexagon have?", ["4", "6","8", "10"], "6"),
+  new Question("How many letters are in the English alphabet?", ["28", "32", "25", "26"], "28"),
+  new Question("What is the name of our galaxy?", ["Canopus", "Sirius","The Nebula", "The Milkyway"], "The Milkyway"),
+  new Question("What is the capitol of the United States of America", ["Philadelphia", "New York City", "Washington D.C", "Chicago"], "Washington D.C"),
+  new Question("How many minutes are in an hour?", ["60", "360", "1", "24"], "60")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
